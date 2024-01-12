ef2fb88be711eead4bc1232ef846ac444f7ea46b
marcos0180 <153757966+marcos0180@users.noreply.github.com>
2024-01-13 05:01:45 +0530

    QB 700.java

---
+++ QB 700.java
@@ -0,0 +1,43 @@
+//QB 700
+
+import java.util.*;
+
+class point {
+    int x, y;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        point p1 = new point();
+        System.out.println("Enter x: ");
+        int a = sc.nextInt();
+        System.out.println("Enter y: ");
+        int b = sc.nextInt();
+
+        point p2 = new point(a, b);
+        point p3 = new point();
+
+        p3.add(p1, p2);
+        p3.display();
+
+    }
+
+    point() {
+        x = 5;
+        y = 5;
+    }
+
+    point(int a1, int b1) {
+        x = a1;
+        y = b1;
+    }
+
+    void add(point a2, point b2) {
+        x = a2.x + b2.x;
+        y = a2.y + b2.y;
+    }
+
+    void display() {
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
