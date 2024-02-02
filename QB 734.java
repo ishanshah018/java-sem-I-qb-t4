9610527e0b64742c127b0f0edb627848d8baa770
marcos0180 <153757966+marcos0180@users.noreply.github.com>
2024-02-03 05:04:20 +0530

    QB 734.java

---
+++ QB 734.java
@@ -0,0 +1,20 @@
+//QB 734
+
+class Ex1 {
+    static int count = 0;
+
+    public static void main(String[] args) {
+        Ex1 o1 = new Ex1();
+        add();
+        Ex1 o2 = new Ex1();
+        add();
+        Ex1 o3 = new Ex1();
+        add();
+
+        System.out.println("Total objects created are: "+count);
+    }
+
+    static void add() {
+        count++;
+    }
+}
